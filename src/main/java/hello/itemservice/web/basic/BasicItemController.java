b268f2b065366bf036139aaf309dfc88fd92c3fd
ressKim <sh21dlat@gmail.com>
2021-05-30 13:12:43 +0900

    상품 등록 처리 - @ModelAttribute 에 대해 study

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -40,10 +37,53 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    //    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
     }
+
+    //@ModelAttribute 에는 item 명칭으로 자동추가가 내부적으로 된다 그래서 안에서 model 추가 안해도 된다.
+    //즉 @ModelAttribute 는 요청 파라미터 처리, Model 추가 라는 역할 두개르 한다.
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+//        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute 의 괄호 안을 생략하면
+     * HelloData -> helloData
+     * Item -> item
+     * model.addAttribute("item", item) 과 같은 형태가 된다.
+     */
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    //@ModelAttribute 는 이 상황에서 생략가능 >> model 에도 그대로 담겨있다.
+    //이 마지막 @ModelAttribute 생략은 취향껏~ 잘 모르는 사람이 팀원에 많을경우 생략 안하는것도 고려해봐야된다.
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
 
     /**
      * test 용 데이터 추가
